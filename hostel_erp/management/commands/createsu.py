5464fe94c4e0318e397aece273de9a055b19305c
nagababu9014 <nagaballa73@gmail.com>
2025-11-16 22:31:33 +0530

    create superuser

---
+++ hostel_erp/management/commands/createsu.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="nagababu",
+                email="ballanagababu29@gmail.com",
+                password="9014884200@Naga"
+            )
+            print("Superuser created")
+        else:
+            print("Superuser already exists")
