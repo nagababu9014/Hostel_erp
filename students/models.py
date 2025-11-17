444f776a180b3f25e0d271b261d062eb511f29a5
nagababu9014 <nagaballa73@gmail.com>
2025-11-17 17:35:03 +0530

    Restore student_email field (nullable) to match migrations

--- students/models.py
+++ students/models.py
@@ -14,7 +14,7 @@ class Student(models.Model):
 
     father_name = models.CharField(max_length=100)
     father_phone_number = models.CharField(max_length=15)
-    student_email = models.EmailField(blank=True, null=True)
+    student_email = models.EmailField(max_length=254, blank=True, null=True)
 
     fees_paid = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     utr_number = models.CharField(max_length=100, null=True, blank=True)
